3b7a4d569dbd8a23fbcee184ebd44fcec6a702bc
IvaPet <agrgrgrgrgrgrgrgr@gmai.com>
2025-02-15 19:19:05 +0300

    7th commit

--- 1/src/Project/Container.js
+++ 1/src/Project/Container.js
@@ -1,18 +1,33 @@
 import React, { useEffect, useState } from 'react'
 
-export default function Container( props ) {
+let textHoverInst = 'card-text'
+let imageHoverInst = 'card-image'
 
+function mouseIn ( ) {
+    textHoverInst += 'hover';
+    imageHoverInst += 'hover';
+    console.log('In');
+}
+function mouseOut ( ) {
+    textHoverInst = 'card-text';
+    imageHoverInst = 'card-image';
+    console.log('Out');
+}
+
+export default function Container( props ) {
+    
     const [ background, setBackground ] = useState( "#5E3F89" )
 
 
-    useEffect(() => {
-        const img = new Image()
-        img.src = props.img[ 0 ]
+    // useEffect(() => {
+    //     const img = new Image()
+    //     img.src = props.img[ 0 ]
 
-        setBackground( props.img[ 1 ] )
+    //     // setBackground( props.img[ 1 ] )
 
-        // setPortrait( img.width < img.height )
-    })
+    //     // setPortrait( img.width < img.height )
+    // })
+
 
     return (
         <div style={{
@@ -22,21 +37,26 @@ export default function Container( props ) {
             minHeight: "200px",
             maxHeight: "300px",
             position: "relative",
-            overflow: "hidden",
+            // overflow: "hidden",
             borderRadius: "20px",
-            background: background,
-            display: "flex",
+            background: "grey",
+            display: "flex", /* ? */
         }}>
-            <div className='card-text'>
+            <div onMouseEnter={ () => { mouseIn( 'text' ) } }
+                onMouseLeave={ () => { mouseOut( 'text' ) } }
+                className={ textHoverInst }>
                 <h5 style={{ position: "absolute", bottom: "0px" }}>Heading</h5>
                 <span>description</span>
                 <button></button>
             </div>
-            <img className="card-image" style={{             
+            <img 
+                onMouseEnter={ () => { mouseIn( 'image' ) } }
+                onMouseLeave={ () => { mouseOut( 'image' ) } }
+                className={ imageHoverInst } style={{   
                 objectFit: "cover",
-                objectPosition: "80% 60%",
+                objectPosition: "center center",
                 borderRadius: "20px"
-            }} src={ props.img[ 0 ] }></img>
+            }} src={ props.img }></img>
         </div>
     )
 }
--- 1/src/Project/Scroll.js
+++ 1/src/Project/Scroll.js
@@ -9,14 +9,8 @@ import dog1 from './imgs/cards/dog1.jpg'
 
 export default function Scroll( props ) {
     const CARDS = [
-        [cat, "white"], 
-        [dog, "#f3f3f3"], 
-        [cat1, "linear-gradient( to right, #f1bbdc, #f3bad9, #ecb2d2, #e3a5c9, #e9adcf )"], 
-        [dog1, "linear-gradient( to bottom, #c47748, #cb7c49, #cd7d4b, #cc7c4a, #c06e40, #be6e3f )"],
-        [cat2, "linear-gradient( to bottom, #db9b9b, #e8abab )"], [cat, "white"], [dog, "#f3f3f3"],
-        [cat1, "linear-gradient( to right, #f1bbdc, #f3bad9, #ecb2d2, #e3a5c9, #e9adcf )"],
-        [dog1, "linear-gradient( to bottom, #c47748, #cb7c49, #cd7d4b, #cc7c4a, #c77745, #cc7b4e, #c06e40, #be6e3f )"],
-        [cat2, "linear-gradient( to bottom, #db9b9b, #e8abab )"]]
+        cat,  dog, cat1, dog1, cat2,  cat, dog, cat1, dog1, cat2, 
+    ]
         
     return (
         <div style={{
