430cb3da7bf3c60408ef3726b6f8f9c7ba3b3d84
IvaPet <151471045+IvaPet266@users.noreply.github.com>
2025-03-15 19:03:50 +0300

    Delete 1 directory

--- 1/src/1/Test2.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Test2( props ) {
-    return (
-        <div style={ { background: "wheat" } }>
-            <p> { props.animals[ props.animals.length-1 ] } </p>
-        </div>
-    )
-}
